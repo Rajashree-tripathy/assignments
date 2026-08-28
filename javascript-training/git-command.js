09c9d636bdb850924e3f458ef1a5a1360281aeda
Biswanath Sahoo <rajashree.tripathy28@gmail.com>
2026-08-28 18:15:06 +0530

    Assignment 1

---
+++ javascript-training/git-command.js
@@ -0,0 +1,12 @@
+//git init => to monitor all changes in the project
+
+//git remote add origin https://github.com/Rajashree-tripathy/assignments.git
+//git config --global user.name"Rajashree-tripathy"
+
+//git config --global user.email "rajashree.tripathy28@gmail.com"
+
+//git add . => to add all the changes in the staging area
+
+//git commit -m "message" =>to commit the changes in the local repository
+
+//git push origin master => to push the changes in the remote repository
---
+++ javascript-training/typescript-training/Assignments/assignment1.ts
@@ -0,0 +1,69 @@
+//1. Temperature of a city in degrees Celsius: 25.5
+let temperature:number = 25.5;
+console.log(`Temperature of a city in degrees Celsius: ${temperature}`);
+
+//2.Whether a customer has placed an order: true or false
+let placedorder:boolean=true;
+console.log(`A customer has placed an order: ${placedorder}`);
+
+//3.Person's phone number: "123-456-7890"
+let phonenumber:string = "123-456-7890";
+console.log(`Person's phone number: ${phonenumber}`) ;
+
+//4.Amount of money in a customer's bank account: 1000.50
+let accountBalance:number=1000.50;
+console.log(`Amount of money in a customer's bank account:${accountBalance}`);
+
+//5.Person's email address: "john.doe@example.com"
+let emailaddress:string="john.doe@example.com";
+console.log(`Email Address ${emailaddress}`);
+
+//6.Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let Coordinates:number [] = [37.7749, -122.4194];
+console.log(`Coordinates of a location ${Coordinates[0]}, longitude: ${Coordinates[1]}`);
+
+//7.Person's marital status: true or false
+let maritalstatus :boolean=true;
+console.log(`marital status: ${maritalstatus}`);
+
+//8.Person's occupation: "Software Engineer"
+let occupation:string ="Software Engineer";
+console.log(`Person's occupation: ${occupation}`);
+
+//9.Person's favourite colour: "Blue"
+let favouriteColour : string = "Blue";
+console.log(`Person's favourite colour: ${favouriteColour}`);
+//10.Current year: 2023
+let CurrentYear:Number=2023;
+console.log(`Current year: ${CurrentYear}`);
+//11.Number of followers on a social media platform: 1,000,000
+let followersCount:string="1,000,000";
+console.log(`Number of followers on a social media platform:${followersCount}`);
+//12.Rating of a movie: 7.5
+let movieRating:number=7.5;
+console.log(`Rating of a movie:${movieRating}`);
+//13.Person's blood type: 'A'
+let bloodtype:string='A';
+console.log(`Person's blood type: ${bloodtype}`);
+//14.Title of a book: "To Kill a Mockingbird"
+let bookTitle:string= "To Kill a Mockingbird";
+console.log(`Title of a book:${bookTitle}`);
+//15.Number of employees in a company: 500
+let employeeCount:number=500;
+console.log(`Number of employees in a company:${employeeCount}`);
+//16.Time of an event: 2:30 PM
+let eventTime:String="2:30 PM";
+console.log(`Time of an event:${eventTime}`);
+//17.Name of a country: "United States"
+let countryName:String="United States";
+console.log(`Name of a country: ${countryName}`);
+//18.Person's eye color: "Brown"
+let eyeColour :string="Brown";
+console.log(`Person eye color: ${eyeColour}`);
+//19.Person's birthplace: "New York City"
+let birthplace:string="New York City";
+console.log(`Person's birthplace:${birthplace}`);
+
+//20. Distance between two cities: 200.5
+let distancebetweencities:number=200.5;
+console.log(`Distance between two cities:${distancebetweencities}`);
